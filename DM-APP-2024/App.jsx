ed6b012cd876594bc3e08d1f02eb39c1d94d9db8
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2023-12-03 23:58:38 -0500

    Converted to JSX from JS

--- DM-APP-2024/App.jsx
+++ DM-APP-2024/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -30,9 +31,6 @@ function App() {
   );
 }
 
-export default App;
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,3 +39,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
