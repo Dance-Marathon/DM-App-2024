08b41ee0fc88b07b0db3940b34d84edf781af577
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2024-03-02 16:04:29 -0500

    updated login page

--- DM-APP-2024/Login.jsx
+++ DM-APP-2024/Login.jsx
@@ -1,7 +1,7 @@
 // Login.js (React Native)
 
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, StyleSheet, Image } from 'react-native';
 import { handleLogin } from './Firebase/AuthManager.js'; // Import the handleLogin function
 
 const Login = () => {
@@ -15,31 +15,39 @@ const Login = () => {
   return (
     <TouchableWithoutFeedback onPress={dismissKeyboard}>
       <View style={styles.container}>
-        <Text style={styles.title}>Login</Text>
-        <View style={styles.inputContainer}>
-          <Text>Email:</Text>
-          <TextInput
-            style={styles.input}
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            placeholder="Enter your email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
+        <Image
+          style={styles.logo}
+          source={require('./images/year30_logo.png')}
+        />
+        <View style={styles.loginbox}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputTop}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              placeholder="Email Address"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
 
-          <Text>Password:</Text>
-          <TextInput
-            style={styles.input}
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            placeholder="Enter your password"
-            secureTextEntry
-          />
+            <TextInput
+              style={styles.inputButtom}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              placeholder="Password"
+              secureTextEntry
+            />
+          </View>
+
+          <TouchableOpacity style={styles.loginButton} onPress={() => handleLogin(email, password)}>
+            <Text style={styles.buttonText}>Log In</Text>
+          </TouchableOpacity>
+
+          <Text>    _______________________________</Text>
+
+          <Text style={styles.forgotPassword}>Forgot password?</Text>
+          <Text style={styles.signUp}>New User?<Text style={{color: '#61A0DA'}}> Sign Up!</Text></Text>
         </View>
-
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -50,30 +58,65 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#233563',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  logoContainer: {
+    marginBottom: 30,
+  },
+  loginbox: {
+    width: '80%',
+    backgroundColor: '#F2EFEE',
+    borderRadius: 10,
+    padding: 20,
+    borderColor: 'black',
+    borderWidth: 5,
   },
   inputContainer: {
-    width: '80%',
     marginBottom: 20,
   },
-  input: {
+  inputTop: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: 'black',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: '#D9D9D9',
+  },
+  inputButtom: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: '#D9D9D9',
   },
   loginButton: {
-    backgroundColor: 'blue',
-    padding: 10,
+    backgroundColor: '#E2883C',
+    padding: 15,
     borderRadius: 5,
+    alignSelf: 'stretch',
+    marginBottom: 10,
   },
   buttonText: {
     color: 'white',
     textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  forgotPassword: {
+    color: '#61A0DA',
+    textAlign: 'center',
+    marginTop: 15,
+  },
+  signUp: {
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 15,
+  },
+  logo: {
+    width: 225,
+    height: 225,
+    marginBottom: 50,
   },
 });
 
